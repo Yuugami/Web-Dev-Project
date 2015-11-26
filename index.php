d76237e98910ae89672757b51542b1451d8b8ee1
root <steven.avery@uoit.net>
2015-11-26 16:34:56 -0500

    added login.html, register.html

--- index.php
+++ index.php
@@ -1,71 +1,71 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Web Dev Project</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" type="text/css" href="css/style.css"/>
-	<!-- Bootstrap -->
-	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
-	<!-- Optional theme -->
-	<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-theme.min.css">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-  <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!--[if lt IE 9]>
-  <script src="../../assets/js/html5shiv.js"></script>
-  <script src="../../assets/js/respond.min.js"></script>
-  <![endif]-->
-  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-  <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>    
-  <!-- Latest compiled and minified JavaScript -->
-  <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+    <title>Web Dev Project</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <!-- Bootstrap -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-theme.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="../../assets/js/html5shiv.js"></script>
+    <script src="../../assets/js/respond.min.js"></script>
+    <![endif]-->
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>    
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
 
-  <script type='text/javascript' src='js/main.js'></script>
+    <script type='text/javascript' src='js/main.js'></script>
 </head>
 <body>
-  <!-- Navigation bar just to take up space. This alphaalphalpha. So alpha it hurts.-->
-  <div class="header">
-    <i class="fa fa-bars fa-3x"></i>
-  </div>
-  <div class="home">
-    <div class="container-fluid">
-     <div class="corebody">
-      <!-- Design of the main portion of the body of the site. -->
-      <div class="row">
-       <div class="col-md-8">
-        <div class="blocks" id="facebook"><iframe id="video" width="100%" height="650" src="//www.youtube.com/embed/7CNpLgwa0-c?rel=0" frameborder="0" allowfullscreen></iframe></div>
-      </div>
-      <div class="col-md-4">
-        <div class="row">
-         <div class="col-md-12">
-          <div class="blocks" id="twitter">
-            <a class="twitter-timeline" width="100%"
-            height="100%" href="https://twitter.com/WindjyEJean" data-widget-id="669301927713177600">Tweets by @WindjyEJean</a>
-          </div>
-        </div>
-        <div class="col-md-12">
-          <div class="blocks" id="instagram">Instagram</div>
-        </div>
-      </div>
+    <!-- Navigation bar just to take up space. This alphaalphalpha. So alpha it hurts.-->
+    <div class="header">
+        <i class="fa fa-bars fa-3x"></i>
     </div>
-  </div>
-  <div class="row">
-   <div class="col-md-12">
-    <div class="blocks">
-     Other Stuff/Feed/RSS:<br>
-     We want to focus on brief details in this section, for example: the score of a game of a sport, breaking local news (you can change sections), game news perhaps you can also link this section to your gmail and get notification here if you wish, etc. etc....
-   </div>
- </div>
-</div>
-<!-- End of the design -->
-</div>
-</div>
-</div>
-<div class="footer">
-  <!--<div class="horibar"></div>
-  <p class="copyright">Copyright © 2015 Windjy Jean, Steven Avery. All rights reserved.</p>-->
-</div>
+    <div class="home">
+        <div class="container-fluid">
+            <div class="corebody">
+                <!-- Design of the main portion of the body of the site. -->
+                <div class="row">
+                    <div class="col-md-8">
+                        <div class="blocks" id="facebook"><iframe id="video" width="100%" height="650" src="//www.youtube.com/embed/7CNpLgwa0-c?rel=0" frameborder="0" allowfullscreen></iframe></div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="blocks" id="twitter">
+                                    <a class="twitter-timeline" width="100%"
+                                    height="100%" href="https://twitter.com/WindjyEJean" data-widget-id="669301927713177600">Tweets by @WindjyEJean</a>
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="blocks" id="instagram">Instagram</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="blocks">
+                            Other Stuff/Feed/RSS:<br>
+                            We want to focus on brief details in this section, for example: the score of a game of a sport, breaking local news (you can change sections), game news perhaps you can also link this section to your gmail and get notification here if you wish, etc. etc....
+                        </div>
+                    </div>
+                </div>
+                <!-- End of the design -->
+            </div>
+        </div>
+    </div>
+    <div class="footer">
+    <!--<div class="horibar"></div>
+    <p class="copyright">Copyright © 2015 Windjy Jean, Steven Avery. All rights reserved.</p>-->
+    </div>
 </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -1,18 +1,57 @@
+
+// adds a tooltip for the registration menus
+function add_tooltip (id, message, onCondition) {
+    // automatically checked the emails match onblur
+    $(id).blur(function () {
+        // if our condition is met
+        if(onCondition() == true) {
+            // add our tool tip
+            $(id).attr("title", message)
+            .tooltip({
+                position: "bottom left",
+                trigger: "manual"
+            }).tooltip("show");
+        } else {
+            // if the field is good, make sure our tooltip isn't showing
+            $(id).tooltip("hide");
+        }
+    })
+    .focus(function () {
+        // get the tool tip out of the way when the user whats to input data
+        $(id).tooltip("destroy");
+    });
+}
+
 $(document).ready(function()
 {
-	console.log("Hey.");
+
+
+    //  tooltip to make sure email addresses match
+    add_tooltip("#verifyEmail", "must match Email address above", function () {
+        // if the email address, doesn't match verifyEmail address, show tool tip
+        return ($("#email").val() != $("#verifyEmail").val());
+    });
+
+    // tooltip to make sure the username isn't taken
+    add_tooltip("#registerContainer #username", "Username unavailable", function () {
+        // TODO: update to compare $("#registerContainer #username").val() to current username list
+        return true;
+    });
+
+    console.log("Hey.");
+
 });
 
 !function(d,s,id)
 {
-	var js,fjs=d.getElementsByTagName(s)[0],
-	p=/^http:/.test(d.location)?'http':'https';
+    var js,fjs=d.getElementsByTagName(s)[0],
+    p=/^http:/.test(d.location)?'http':'https';
 
-	if(!d.getElementById(id))
-		{
-			js=d.createElement(s);
-			js.id=id;
-			js.src=p+"://platform.twitter.com/widgets.js";
-			fjs.parentNode.insertBefore(js,fjs);
-		}
+    if(!d.getElementById(id))
+        {
+            js=d.createElement(s);
+            js.id=id;
+            js.src=p+"://platform.twitter.com/widgets.js";
+            fjs.parentNode.insertBefore(js,fjs);
+        }
 }(document,"script","twitter-wjs");
